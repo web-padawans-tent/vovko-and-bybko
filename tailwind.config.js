a1f3b7981f76e17db2e355abb549ec2279f28036
Zakhar <zakhar.bybko@icloud.com>
2025-01-12 17:14:06 +0100

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './app.vue',
+    './pages/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
